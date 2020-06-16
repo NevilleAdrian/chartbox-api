f6b51748eb949755c2f36f94a2b1d390c812847e
nevilleadrian <nchukumah@infomall.ng>
2020-06-16 20:34:32 +0100

    done

--- dbconnection.js
+++ dbconnection.js
@@ -1,5 +1,6 @@
 const  mongoose  = require("mongoose");
 mongoose.Promise  = require("bluebird");
 const  url  =  "mongodb://localhost:27017/chat";
+const uri = "mongodb+srv://neville:osaseretope@cluster0.loeg5.mongodb.net/chat?retryWrites=true&w=majority"
 const  connect  =  mongoose.connect(url,  { useNewUrlParser: true ,useUnifiedTopology: true, useCreateIndex: true});
 module.exports  =  connect;
